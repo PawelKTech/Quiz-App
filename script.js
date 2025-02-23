0d00b778dbfa48b1dcbfc23395148a36de2ad87c
PawelKTech <pawel.kozanecki.06@gmail.com>
2025-02-23 23:39:04 +0100

    App is working but still need UI improvement

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector("#questionContent");
 const button = document.querySelector("#checkAnswerButton");
-console.log(button);
 let parentDiv = button.parentNode;
 
 async function getQeustions() {
@@ -14,15 +13,15 @@ async function getQeustions() {
     }
 
     const json = await response.json();
-    displayQeustions(json);
+    return json;
   } catch (e) {
     console.error(e);
   }
 }
 
-getQeustions();
-
-function displayQeustions(json) {
+async function displayQeustions() {
+  let json = await getQeustions();
+  console.log(json);
   json.forEach((question, questionIndex) => {
     const div = document.createElement("div");
     const title = document.createElement("h3");
@@ -45,4 +44,52 @@ function displayQeustions(json) {
       parentDiv.insertBefore(div, button);
     });
   });
+}
+
+displayQeustions();
+
+button.addEventListener("click", async (event) => {
+  let userAnswers = {};
+  let inputs = document.querySelectorAll('input[type="radio"]');
+
+  inputs.forEach((radio) => {
+    if (radio.checked) {
+      userAnswers[radio.name] = radio.value;
+      console.log(radio);
+    } else {
+      if (!userAnswers[radio.name]) {
+        userAnswers[radio.name] = null;
+      }
+    }
+  });
+
+  let question = await getQeustions();
+  let correctAnswer = [];
+  let result = 0;
+  question.forEach((q, index) => {
+    correctAnswer.push(q.correct);
+  });
+
+  correctAnswer.forEach((answer, index) => {
+    if (answer == userAnswers[index]) {
+      result++;
+    }
+  });
+
+  inputs.forEach((input) => {
+    input.disabled = true;
+  });
+
+  container.innerHTML = `<h1>
+  Good job you did:
+  ${result}</h1>
+
+  <button onclick='resetQuestion()'>Again</button>
+  <a href="index.html">
+    <button>Wróc do strony głowen </button></a>`;
+  console.log(result);
+});
+
+function resetQuestion() {
+  window.location.reload();
 }
